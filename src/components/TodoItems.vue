840fc6786c673459179255b66ea5228f16937f8e
Andre Costa <andre2396costa@gmail.com>
2022-11-20 10:24:31 -0300

    Projeto todo-list vuejs

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="space-y-2">
+      <TodoItem v-for="todo in $store.state.todos" v-bind:key="todo.id" :todo="todo" />
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+export default {
+  components: { TodoItem },
+
+}
+</script>
+
+<style>
+
+</style>
